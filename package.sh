471a78a2fe1a673c2850138a840c216ef27ae326
Laurens Hoogenboom <lphoogenboom@protonmail.com>
2024-06-03 16:16:29 +0200

    removes depricated file

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
-#!usr/bin/env bash
+#!/bin/bash
 
 # Prompt for the version number
-read -p "Enter the version number (e.g., 1.0.2): " version_number
+read -p "Enter the version number (e.g., 0.0.1): " version_number
 
-# Create the package named fayufox-[version_number].xpi excluding shell.nix, package.sh, and hidden files/folders
-echo "Packaging all files and folders into fayufox-${version_number}.xpi, excluding shell.nix, package.sh, and hidden files/folders..."
-zip -r "fayufox-${version_number}.xpi" . -x shell.nix -x package.sh -x ".*" -x "*/.*"
+# Create the package named fayufox-[version_number].xpi excluding shell.nix, package.sh, and hidden files/folders using Deflate algorithm
+echo "Packaging all files and folders into fayufox-${version_number}.xpi, excluding shell.nix, package.sh, and hidden files/folders using Deflate algorithm..."
+zip -r -Z deflate -FS "fayufox-${version_number}.zip" . -x shell.nix -x package.sh -x ".*" -x "*/.*"
 echo "Packaging complete."
