89ebd213be021a4c0d2fa2a055587d7191c8501d
Laurens Hoogenboom <lphoogenboom@protonmail.com>
2024-06-04 16:17:25 +0200

    updates packager

--- package.sh
+++ package.sh
@@ -5,5 +5,5 @@ read -p "Enter the version number (e.g., 0.0.1): " version_number
 
 # Create the package named fayufox-[version_number].xpi excluding shell.nix, package.sh, and hidden files/folders using Deflate algorithm
 echo "Packaging all files and folders into fayufox-${version_number}.xpi, excluding shell.nix, package.sh, and hidden files/folders using Deflate algorithm..."
-zip -r -Z deflate -FS "fayufox-${version_number}.zip" . -x shell.nix -x package.sh -x ".*" -x "*/.*"
+zip -r -Z deflate -FS "fayufox-${version_number}.xpi" . -x shell.nix -x package.sh -x ".*" -x "*/.*"
 echo "Packaging complete."
